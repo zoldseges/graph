5f05fdad0af5eeacee5524db4182c22a7d7b2a64
Palkovics Adam <pa.adam94@gmail.com>
2021-10-07 23:43:25 +0200

    button introduced

--- src/main.c
+++ src/main.c
@@ -19,14 +19,51 @@ void * p_sec(void * code)
   return code;
 }
 
-struct App{
-  enum states{
-	     QUIT,
-	     RUN
-  }state;
+enum states{
+	    QUIT,
+	    RUN,
+	    ADD_NODE
+} state;
+
+struct Button{
+  int x, y, w, h;
+  SDL_Color color;
+  void (*callback)(void);
 };
 
+void buttontest(){
+  printf("ohh yah\n");
+}
+
 int main(int argc, char *argv[]){
+  struct Button button1 = {
+			  400,
+			  400,
+			  80,
+			  40,
+			  { 0xFF,
+			    0xFF,
+			    0xFF,
+			    0xFF
+			  },
+			  buttontest
+  };
+
+  struct Button button2 = {
+			  500,
+			  400,
+			  80,
+			  40,
+			  { 0xFF,
+			    0xFF,
+			    0xFF,
+			    0xFF
+			  },
+			  buttontest
+  };
+
+  void *elements[] = { &button1, &button2 };
+
   i_sec(SDL_Init(SDL_INIT_VIDEO));
   SDL_Window *window = p_sec(SDL_CreateWindow("Graph",
 					   0,0,800,600,
@@ -35,20 +72,37 @@ int main(int argc, char *argv[]){
     p_sec(SDL_CreateRenderer(window,
 			     -1,
 			     SDL_RENDERER_ACCELERATED));
-  struct App app = {
-		    .state = RUN,	    
-  };
   SDL_Event event;
-  while(app.state != QUIT){
+  state = RUN;
+  while(state != QUIT){
     while(SDL_PollEvent(&event))
       {
 	switch(event.type){
 	case SDL_QUIT: {
-	  app.state = QUIT;
+	  state = QUIT;
 	}break;
 	}
       }
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
+    for( int i = 0; i < 2; i++){
+      struct Button *btn = ((struct Button*)elements[i]);
+      struct SDL_Rect rect = {
+			      btn->x,
+			      btn->y,
+			      btn->w,
+			      btn->h
+      };
+      SDL_SetRenderDrawColor(renderer, btn->color.r,
+			     btn->color.g,
+			     btn->color.b,
+			     btn->color.a);
+      SDL_RenderFillRect(renderer, &rect);
+      
+    }
+    SDL_RenderPresent(renderer);
   }
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
