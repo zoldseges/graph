967a99caba448874ea08a0638e26b96c49f87f0b
palkovics adam <pa.adam94@gmail.com>
2022-03-13 11:10:41 +0100

    move "node functions" from model.c to controller.c

--- src/controller.h
+++ src/controller.h
@@ -5,6 +5,8 @@
 #include "view.h"
 
 void init_ctl(Ctl *ctl);
+void ctl_handler(Ctl *ctl);
+void free_ctl(Ctl *ctl);
 
 enum MARKED marked_type(Marked *marked);
 
@@ -19,8 +21,40 @@ void marked_call(void (*f)(Node *node[2], gpointer d),
 		 gpointer d);
 
 
-void ctl_handler(Ctl *ctl);
+Node *get_node_by_id(Graph *graph, int id);
 
-void free_ctl(Ctl *ctl);
+/* runs a function on all nodes of a graph
+ * function signature:
+ *   void f(Node *node, gpointer data);
+ */
+void nodes_call(void (*f)(Node *n, gpointer d),
+		Graph *g,
+		gpointer d);
 
+/* runs a function which returns an integer
+ * on all nodes, selects the Node with which
+ * the function returned with the smallest
+ * POSITIVE integer and returns a pointer
+ * to it.
+ * function signature:
+ *   int f(Node *node, gpointer data);
+ */
+Node *nodes_filter_one(int (*f)(Node *n, gpointer d),
+		       Graph *g,
+		       gpointer d);
+
+
+// TODO
+/*
+ * runs a function which returns a bool
+ * on all nodes, selects all Nodes with
+ * which the function returned true and returns
+ * their ID in an array structured as following
+ * {number of matches; id 1; id 2... }
+ * function signature:
+ *   gboolean f(Node *node, gpointer data);
+ */
+Node *nodes_filter_all(gboolean (*f)(Node *n, gpointer d),
+		       Graph *g,
+		       gpointer d);
 #endif /* CONTROLLER_H__ */
--- src/model.h
+++ src/model.h
@@ -12,41 +12,4 @@ void move_node(Graph *graph, Node *node, Point p);
  */
 void set_edge_weight(Graph *graph, Node *from, Node *to, int weight);
 
-Node *select_node(Graph *graph, int id);
-
-/* runs a function on all nodes of a graph
- * function signature:
- *   void f(Node *node, gpointer data);
- */
-void nodes_call(void (*f)(Node *n, gpointer d),
-		Graph *g,
-		gpointer d);
-
-/* runs a function which returns an integer
- * on all nodes, selects the Node with which
- * the function returned with the smallest
- * POSITIVE integer and returns a pointer
- * to it.
- * function signature:
- *   int f(Node *node, gpointer data);
- */
-Node *nodes_filter_one(int (*f)(Node *n, gpointer d),
-		       Graph *g,
-		       gpointer d);
-
-
-// TODO
-/*
- * runs a function which returns a bool
- * on all nodes, selects all Nodes with
- * which the function returned true and returns
- * their ID in an array structured as following
- * {number of matches; id 1; id 2... }
- * function signature:
- *   gboolean f(Node *node, gpointer data);
- */
-Node *nodes_filter_all(gboolean (*f)(Node *n, gpointer d),
-		       Graph *g,
-		       gpointer d);
-
 #endif /* MODEL_H__ */
